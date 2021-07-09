0e4cbac3ca5af345babc34a4e5683659e978e0f0
HaoYang670 <yanghao@DESKTOP-O9FDBAG.localdomain>
2021-07-09 10:34:46 +0800

    Lab0 webget: pass all tests

--- apps/webget.cc
+++ apps/webget.cc
@@ -11,14 +11,25 @@ void get_URL(const string &host, const string &path) {
 
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
+    TCPSocket sock;
+    sock.connect(Address(host, "http"));
+
     // then request the URL path given in the "path" string.
+    const string request = "GET " + path + " HTTP/1.1\r\n" + 
+                            "Host: " + host + "\r\n" +
+                            "Connection: close \r\n\r\n";
+    sock.write(request);
 
     // Then you'll need to print out everything the server sends back,
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
+    while(!sock.eof()){
+        cout << sock.read();
+    }
+    sock.close();
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    //cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    //cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
