c84daa057a602b460c829318a40b16a878dec509
HaoYang670 <yanghao@DESKTOP-O9FDBAG.localdomain>
2021-07-18 16:13:55 +0800

    Lab2: pass all tests

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,19 +1,47 @@
 #include "tcp_receiver.hh"
-
+#include <iostream>
 // Dummy implementation of a TCP receiver
 
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+//template <typename... Targs>
+//void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    //DUMMY_CODE(seg);
+    if(_reassembler.stream_out().input_ended()){
+        return;
+    }
+
+    const TCPHeader header = seg.header();
+
+    if(!_hasSYN && header.syn) {
+        _hasSYN = true;
+        _isn = header.seqno;
+    }
+    if(_hasSYN) {
+        if(header.fin){
+            _hasFIN = true;
+        }
+        const uint64_t absIdx = unwrap(header.seqno, _isn, _reassembler.stream_out().bytes_written() + 1);
+        const uint64_t streamIdx = absIdx > 0 ? absIdx-1 : 0;
+        const string content = seg.payload().copy();
+        _reassembler.push_substring(content, streamIdx, header.fin);
+    }
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const { 
+    if (!_hasSYN) return {};
+    else {
+        uint ackIdx = _reassembler.stream_out().bytes_written() + 1;  // plus SYN flag
+        if(_reassembler.stream_out().input_ended()) ackIdx ++; // plus FIN flag
+        return {wrap(ackIdx, _isn)};
+    }
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { 
+    return _reassembler.stream_out().remaining_capacity(); 
+}
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -20,12 +20,21 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    bool _hasSYN;
+    bool _hasFIN;
+    WrappingInt32 _isn;
+
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : 
+      _reassembler(capacity), 
+      _capacity(capacity),
+      _hasSYN(false),
+      _hasFIN(false),
+      _isn(0) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
